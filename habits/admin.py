231f73520dfa2c58be122f39277ff4eb59ab8b5d
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-11-01 02:22:05 +0300

    updated admin.py

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit, Weekday
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'action', 'owner', 'is_public')
+
+
+@admin.register(Weekday)
+class WeekdayAdmin(admin.ModelAdmin):
+    list_display = ('id', 'day', 'number')
