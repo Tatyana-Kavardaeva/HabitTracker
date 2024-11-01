5a6d85bce1b78a68b5077dee0bf7b4fb20e6c23a
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-11-01 03:13:18 +0300

    updated services.py

--- habits/services.py
+++ habits/services.py
@@ -7,6 +7,4 @@ def send_telegram_message(chat_id, message):
         'text': message,
         'chat_id': chat_id,
     }
-    response = requests.get(f'{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage', params=params)
-
-    print(response.json())
+    requests.get(f'{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage', params=params)
